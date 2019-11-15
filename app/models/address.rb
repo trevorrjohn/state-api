8fc99f050b87dbdac32ffbc2cc0470e011e94664
Trevor John <trevor@john.tj>
2019-11-15 10:47:56 -0500

    Upcase and Strip state and country before saving

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,4 +4,14 @@ class Address < ApplicationRecord
   validates :state, presence: true
   validates :city, presence: true
   validates :country, presence: true
+
+  before_validation -> { upcase_and_strip(:state, :country) }
+
+  private
+
+  def upcase_and_strip(*attrs)
+    attrs.each do |attr|
+      public_send("#{attr}=", public_send(attr)&.upcase&.strip)
+    end
+  end
 end
--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "saving country and state are stripped and upcased" do
+    subject = Address.create!(
+      name: "Work",
+      street: "123 Foobar",
+      city: "Denver",
+      state: " co  ",
+      country:  "usa "
+    )
+    assert_equal "CO", subject.state
+    assert_equal "USA", subject.country
+  end
 end

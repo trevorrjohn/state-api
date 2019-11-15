9a103a4eaa7ef958043697f5b9a47d75d9b4a999
Trevor John <trevor@john.tj>
2019-11-15 10:14:51 -0500

    Add endpoint to create an address
    - does not test if the country is valid

--- app/controllers/api/v1/addresses_controller.rb
+++ app/controllers/api/v1/addresses_controller.rb
@@ -1,11 +1,27 @@
 module Api
   module V1
     class AddressesController < ApplicationController
+      def create
+        address = Address.create(address_params)
+        if address.persisted?
+          render status: :created, json: address.as_json
+        else
+          render status: :unprocessable_entity, json: {
+            errors: address.errors.full_messages
+          }
+        end
+      end
 
       def destroy
         address = Address.find(params[:id])
         address.destroy
-        render :ok
+        render status: :ok
+      end
+
+      private
+
+      def address_params
+        params.require(:address).permit(:name, :street, :city, :state, :country)
       end
     end
   end
--- test/controllers/api/v1/addresses_controller_test.rb
+++ test/controllers/api/v1/addresses_controller_test.rb
@@ -18,6 +18,40 @@ module Api
 
         assert_response :not_found
       end
+
+      test "create an address" do
+        body = {
+          address: {
+            name: "Home",
+            street: "24 Park Ave",
+            city: "New York",
+            state: "NY",
+            country: "USA"
+          }
+        }
+        post api_v1_addresses_url(params: body)
+
+        assert_response :created
+        address = Address.order(:created_at).last
+        assert_equal address.to_json, @response.body
+      end
+
+      test "bad address is not created" do
+        body = {
+          address: {
+            name: "",
+            street: "24 Park Ave",
+            city: "New York",
+            state: "NY",
+            country: "USA"
+          }
+        }
+        post api_v1_addresses_url(params: body)
+
+        assert_response :unprocessable_entity
+        errors = { errors: ["Name can't be blank"] }
+        assert_equal errors.to_json, @response.body
+      end
     end
   end
 end
